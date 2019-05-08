f180d7a28348edbbfcf97f625f1be431db1cd68c
Pedro Enju <pedro_enju@hotmail.com>
2019-05-07 22:30:21 -0300

    Funciona, mas está estranho.

--- config/config.routes.php
+++ config/config.routes.php
@@ -5,10 +5,12 @@ namespace Symfony\Component\Routing\Loader\Configurator;
 return function (RoutingConfigurator $routes)
 {
 
-    $routes->add('home', '/')->controller(['MyCode\Home', 'helloWorld']);
+    $routes->add('home', '/')->controller(['MyCode\Controller\Home', 'helloWorld']);
 
-    $routes->add('produto', '/produto')->controller(['MyCode\Produto', 'listarProdutos']);
+    $routes->add('produto', '/produto')->controller(['MyCode\Controller\Produto', 'listarProdutos']);
 
-    $routes->add('sobre', '/sobre')->controller(['MyCode\Sobre', 'index']);
+    $routes->add('sobre', '/sobre')->controller(['MyCode\Controller\Sobre', 'index']);
+
+    $routes->add('coffe', '/cafe/listar')->controller(['MyCode\Controller\Coffe', 'listarCafes']);
 
 };
